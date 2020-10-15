edee05056bf0e834b5f4f2f78151bbd7580d238d
hamedpa <hamedpa21@gmail.com>
2020-10-15 21:19:11 +0330

    remove extra code

--- telegram_bot/t.py
+++
@@ -1,9 +0,0 @@
-import json
-
-data_path = "../digital_currencies.json"
-
-with open(data_path) as json_file:
-        data = json.load(json_file)
-        for p in data['digital_currencies']:
-            if("BTC" in p['asset']):
-               print(p)
